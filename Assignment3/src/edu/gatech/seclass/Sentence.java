5815f50173cf4327846f0b7d0f483b2d449aeafa
Uma Jaipradeep <ujaipradeep3@gatech.edu>
2015-01-21 19:26:16 -0500

    Initial Import

---
+++ Assignment3/src/edu/gatech/seclass/Sentence.java
@@ -0,0 +1,49 @@
+package edu.gatech.seclass;
+
+public class Sentence implements SentenceInterface {
+
+	@Override
+	public void setSentence(String sentence) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getSentence() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int length() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getWord(int position) throws IllegalArgumentException,
+			PositionOutOfBoundsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String[] getWords(int startPosition, int endPosition)
+			throws IllegalArgumentException, PositionOutOfBoundsException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int indexOf(String word) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void reverse() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
